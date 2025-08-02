e596d74d77d44611a1eeb4c690da3c2102498c13
Rishi Rana <rishirana012354@gmail.com>
2025-08-02 22:19:33 +0530

    d

---
+++ Array/leet/Moore_voting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {1, 2, 2, 1, 1};
+    int n = nums.size();
+
+    int freq =0, ans=0;
+    for (int i = 0; i < n; i++)
+    {
+        if (freq==0)
+        {
+            ans= nums[i];
+        }
+        if (ans==nums[i])
+        {
+            freq++;
+        }
+        else{
+            freq--;
+        }
+        
+    }
+      cout<<ans<<endl; 
+      
+}
