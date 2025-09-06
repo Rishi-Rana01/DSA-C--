19a1762efac9984c36c7cb0d862965e6f7a06ab4
Rishi Rana <rishirana012354@gmail.com>
2025-09-06 19:21:13 +0530

    Prime

---
+++ Maths/Prime.cpp
@@ -0,0 +1,26 @@
+// total prime numbers up to n
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int countPrimes(int n) {
+    int count = 0;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) count++;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << countPrimes(n) << endl;
+    return 0;
+}
