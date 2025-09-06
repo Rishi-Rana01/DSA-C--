36cb76301f82ca82469b9aace165de412e2fde7b
Rishi Rana <rishirana012354@gmail.com>
2025-09-06 19:32:36 +0530

    GCD

---
+++ Maths/GCD.cpp
@@ -0,0 +1,18 @@
+// GCD of two numbers
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a = 56, b = 98;
+    cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+    return 0;
+}
