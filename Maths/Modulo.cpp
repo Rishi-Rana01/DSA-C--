369e4472c3bd4674e22b989d6d63ccc63fa5c65a
Rishi Rana <rishirana012354@gmail.com>
2025-09-10 18:15:28 +0530

    2D

---
+++ Maths/Modulo.cpp
@@ -0,0 +1,9 @@
+// Modulo Arithmetic
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 3;
+    cout << "Modulo of " << a << " and " << b << " is " << a % b << endl;
+    return 0;
+}
